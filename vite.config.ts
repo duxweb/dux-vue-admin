5a7611f0d1bb4af3fef7d21cbd8f5ccdca1913d7
mr.l <admin@duxphp.com>
2024-09-27 11:02:03 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,11 @@ import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
-import viteDts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
     vue(),
     VueJsx(),
-    viteDts({
-      insertTypesEntry: true,
-      staticImport: true,
-    }),
   ],
   resolve: {
     alias: {
