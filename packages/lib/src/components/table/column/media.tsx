b3703adb8d1457dd07d447d8921489d50624c537
mr.l <admin@duxphp.com>
2024-12-12 16:06:44 +0800

    增加 media 回调配置

--- packages/lib/src/components/table/column/media.tsx
+++ packages/lib/src/components/table/column/media.tsx
@@ -1,3 +1,4 @@
+import type { VNode } from 'vue'
 import type { TableColumnRender } from '../../table'
 import { NAvatar, NImage } from 'naive-ui'
 import placeholder from '../../../static/images/placeholder.png'
@@ -6,8 +7,8 @@ export interface ColumnMediaProps {
   image?: string
   images?: string[]
   avatar?: string
-  title?: string
-  desc?: string
+  title?: string | ((rowData: Record<string, any>) => VNode)
+  desc?: string | ((rowData: Record<string, any>) => VNode)
   descs?: string[]
   imageWidth?: number
   imageHeight?: number
@@ -15,6 +16,8 @@ export interface ColumnMediaProps {
 
 export function columnMedia({ imageWidth = 36, imageHeight = 36, ...props }: ColumnMediaProps): TableColumnRender {
   return (rowData) => {
+    const title = typeof props.title === 'function' ? props.title(rowData) : rowData[props.title || '']
+    const desc = typeof props.desc === 'function' ? props.desc(rowData) : rowData[props.desc || '']
     return (
       <div class="flex flex-row flex-nowrap gap-2 items-center leading-4">
         {props.avatar && (
@@ -26,13 +29,13 @@ export function columnMedia({ imageWidth = 36, imageHeight = 36, ...props }: Col
             round
             size={imageHeight}
           >
-            {rowData[props.title || '']?.charAt?.(0)}
+            {title?.charAt?.(0)}
           </NAvatar>
         )}
         {props.image && <div><NImage src={rowData[props.image] || placeholder} fallbackSrc={placeholder} objectFit="cover" width={imageWidth} height={imageHeight} /></div>}
         <div class="flex-1 w-1 flex flex-col gap-1">
-          {props.title && <div class="truncate" title={rowData[props.title]}>{rowData[props.title]}</div>}
-          {props.desc && <div class="text-gray-6 truncate" title={rowData[props.desc]}>{rowData[props.desc]}</div>}
+          {props.title && <div class="truncate" title={title}>{title}</div>}
+          {props.desc && <div class="text-gray-6 truncate" title={desc}>{desc}</div>}
           {props.descs && props.descs.map(desc => <div class="text-gray-4 truncate" title={rowData[desc]}>{rowData[desc]}</div>)}
         </div>
       </div>
